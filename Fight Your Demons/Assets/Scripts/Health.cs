17d0757dab7d7a4d83368b8830b080916e91de33
Thaminator <jjwtham@gmail.com>
2020-08-04 14:42:33 +0100

    Health System

---
+++ Fight Your Demons/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    public void DealDamage(float damage)
+    {
+        health-=damage;
+        if(health<=0)
+        { 
+            Destroy(gameObject); 
+        }
+    }
+
+}
