8c2594ccfe5f06a750bb0d4cc1fc1ac7c8f928fb
Thaminator <jjwtham@gmail.com>
2020-08-05 15:15:10 +0100

    Creating UI

---
+++ Fight Your Demons/Assets/DefenderButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        LightUp();
+    }
+
+
+    private void LightUp()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
+        }
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Fight Your Demons/Assets/Scripts/Defender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+
+    [SerializeField] int foodCost = 100;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
