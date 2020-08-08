faa43c7f32b2e5a5369da3dc5f1b882bcaeb0fd5
Thaminator <jjwtham@gmail.com>
2020-08-08 13:57:18 +0100

    Food Display

---
+++ Fight Your Demons/Assets/Scripts/FoodDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FoodDisplay : MonoBehaviour
+{
+    [SerializeField] int food = 100;
+    TextMeshProUGUI foodText;
+
+    private void Start()
+    {
+        foodText = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        foodText.text = food.ToString();
+    }
+
+    public void AddFood(int amount)
+    {
+        food += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendFood(int amount)
+    {
+        if (food >= amount)
+        {
+            food -= amount;
+            UpdateDisplay();
+        }
+        
+
+
+    }
+
+
+}
